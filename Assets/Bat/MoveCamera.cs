6fd6b2ac9e4e16e198ce2e674718db29a1f38899
Tyler Runions <tyler_runs@hotmail.com>
2016-12-14 09:52:26 -0500

    Add files via upload

---
+++ Assets/Bat/MoveCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+
+		var speed = 3.0;
+		if (Input.GetKey("q")) {
+	   transform.Translate(Vector3.left*Time.deltaTime*6f);
+        }else if (Input.GetKey("e"))
+        {
+		transform.Translate(Vector3.right*Time.deltaTime*6f);      
+		}
+    }
+}
